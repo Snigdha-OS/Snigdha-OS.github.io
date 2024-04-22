abf65d05efdbbd2ca2bb79fb3b5d49268da51cbe
iconized <157954129+iconized@users.noreply.github.com>
2024-04-22 05:54:07 +0530

    👽 @iconized made a new commit!

---
+++ iconized.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+# EXECUTE AT YOUR OWN RISK
+
+# Git Config
+git config --global user.name "iconized"
+git config --global user.email "157954129+iconized@users.noreply.github.com"
+
+# Git Push
+git add .
+git commit -m "👽 @iconized made a new commit!"
+git push origin master
+

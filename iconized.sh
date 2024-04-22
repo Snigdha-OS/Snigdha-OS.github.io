1cf14a6b494438ad2bab331eb48afade2620ea41
Eshan Roy (Eshanized) <m.eshanized@gmail.com>
2024-04-22 06:13:05 +0530

    ⏳ @eshanized updated the Website!!!

--- iconized.sh
+++ iconized.sh
@@ -2,12 +2,8 @@
 
 # EXECUTE AT YOUR OWN RISK
 
-# Git Config
-git config --global user.name "iconized"
-git config --global user.email "157954129+iconized@users.noreply.github.com"
-
 # Git Push
 git add .
 git commit -m "👽 @iconized made a new commit!"
-# git push origin master
+git push origin master
 

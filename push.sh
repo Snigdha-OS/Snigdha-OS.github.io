4878325171a58cc05403c852a5e1e848244929d6
Abhiraj Roy <157954129+iconized@users.noreply.github.com>
2024-06-05 04:23:35 +0530

    :racehorse: perf(script): remove @eshanized script tmp
    Signed-off-by: Abhiraj Roy <157954129+iconized@users.noreply.github.com>

--- push.sh
+++ push.sh
@@ -1,31 +1,20 @@
 #!/bin/bash
 
-# Author    : Eshan Roy
-# URI       : https://eshanized.github.io
+# Iconized
 
-# NOTE: If you are on Snigdha OS,
-# you can install commitizen-go with `sudo pacman -S commitizen-go`
-# or `s commitizen-go`. Else you need to install `yay` or `yay-bin`
-# to install commitizen. I have written this script only for *Arch Linux.
-
-# Function to check if Commitizen is installed
-check_commitizen() {
-    if ! pacman -Qq commitizen-go &> /dev/null; then
-        echo "Commitizen is not installed. Please install it using 'yay -S commitizen-go'." >&2
-        exit 1
-    fi
+pull_from_github(){
+    git pull
 }
 
-# Function to stage, commit, and push changes
 push_to_github() {
+    # git pull
     git add .
-    git cz
-    git push origin master
+    ezcommits
+    git push -u origin master
 }
 
-# Main Function
-main() {
-    check_commitizen
+main(){
+    pull_from_github
     push_to_github
 }
 

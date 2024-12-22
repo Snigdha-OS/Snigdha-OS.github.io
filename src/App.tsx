f4f540ae2f645d850a86dc8aa2a60ee6185d7f43
eshanized <m.eshanized@gmail.com>
2024-12-23 05:26:35 +0530

    🐛 fix(_route): browser route => hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Layout } from './components/layout/Layout';
 import { Home } from './pages/Home';
 import { About } from './pages/About';

e8a0ff93d3672a093f19195b205e6b6c3e9f3316
d3v1l0n <d3v1l0n@outlook.in>
2024-12-23 22:20:54 +0530

    🐛 fix: sitmap is not required

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import sitemap from 'vite-plugin-sitemap';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // Correct base URL for GitHub Pages deployment
+  base: '/', 
   plugins: [
-    react(), 
-    sitemap({
-      hostname: 'https://snigdha-os.github.io', // Ensure this is your site URL
-      outDir: 'dist', // Correct build output directory
-      // Optional: specify other options like exclude routes or change frequency if needed
-    }),
+    react(),
   ],
   optimizeDeps: {
-    exclude: ['lucide-react'], // Prevent 'lucide-react' from being pre-bundled
+    exclude: ['lucide-react'], 
   },
 });

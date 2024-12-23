8117bb45e69ab55065d38cb91c3b310b476c2104
eshanized <m.eshanized@gmail.com>
2024-12-23 10:32:57 +0530

    🐛 fix(router): has to browser

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Layout } from './components/layout/Layout';
 import { Home } from './pages/Home';
 import { About } from './pages/About';

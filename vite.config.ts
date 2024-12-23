19c45de63b168f6dec1769f9af4a66967a2deadc
d3v1l0n <d3v1l0n@outlook.in>
2024-12-23 22:51:25 +0530

    ⚡️ perf: vite config utilized

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: '/', 
@@ -8,5 +9,17 @@ export default defineConfig({
   ],
   optimizeDeps: {
     exclude: ['lucide-react'], 
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "@/styles/variables.scss";',
+      },
+    },
   },
 });

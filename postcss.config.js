59f12ba070669ae7723cbc55580b67a4fd3e4b85
d3v1l0n <d3v1l0n@outlook.in>
2024-12-23 22:43:23 +0530

    ⚡️ perf: production level nano css

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,12 @@ export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production'
+      ? {
+          cssnano: {
+            preset: 'default',
+          },
+        }
+      : {}),
   },
 };

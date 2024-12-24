2340d0f691986fbff5e2ad5de7c83874cc6e2b53
eshanized <m.eshanized@gmail.com>
2024-12-25 04:57:02 +0530

    🧹 chore(clean): whole repository

--- src/lib/currency.ts
+++
@@ -1,8 +0,0 @@
-export function formatINR(amount: number) {
-  return new Intl.NumberFormat('en-IN', {
-    style: 'currency',
-    currency: 'INR',
-    minimumFractionDigits: 0,
-    maximumFractionDigits: 0,
-  }).format(amount);
-}
--- src/types/sponsor.ts
+++
@@ -1,6 +0,0 @@
-export interface Sponsor {
-  name: string;
-  description: string;
-  githubUsername: string;
-  amount: number;
-}
